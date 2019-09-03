23f7359b3f0c7c9d2ae9ce396f083573aea671df
nabanitadattase <47370928+nabanitadattase@users.noreply.github.com>
2019-09-03 11:05:45 +0200

    End Points implementation Assignment

--- Week1/End-Point-Implementation-NODE JS/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { createServer } = require('./server');
-
-const PORT = 3016;
-
-createServer().listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
